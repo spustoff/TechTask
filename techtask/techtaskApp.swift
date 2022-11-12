f210a57e7bcc5b58925fb00830ec11e5737b3b76
cxcc <cxzczx>
2022-11-12 21:08:53 +0300

    Initial Commit

---
+++ techtask/techtaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  techtaskApp.swift
+//  techtask
+//
+//  Created by Вячеслав on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct techtaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
